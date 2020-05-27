104beca4fcd6f1e6b1c02e7e5f02ad3bc9f0b480
misui <misui1102@163.com>
2020-05-28 02:05:55 +0800

    optimized HttpRequest and HttpResponse Without used

---
+++ PCClient/LogingWindow/ToolClass/HttpRequest.cs
@@ -0,0 +1,79 @@
+﻿using PCClintSoftware.ToolClass;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogingWindow.ToolClass
+{
+    class HttpRequest
+    {
+        private static string session = "";
+        private UTF8Encoding encoding = new UTF8Encoding(false);   //注明utf-8去除BOM
+
+        private HttpWebRequest httpReq = null;
+
+
+        private HttpResponse response = null;
+
+        public HttpRequest(string urlStr)
+        {
+            httpReq = (HttpWebRequest)WebRequest.Create(new Uri(urlStr));
+            setMethod(HttpMethod.POST);
+            httpReq.Timeout = 10000;
+            httpReq.ProtocolVersion = HttpVersion.Version11;
+            setContentType("application/json");
+            //TODO 部分情况下需要将session清空
+            setSession(session);
+        }
+
+        public void setMethod(string method)
+        {
+            httpReq.Method = method;
+        }
+
+        public void setContentType(string contentType)
+        {
+            httpReq.ContentType = contentType;
+        }
+
+        private void setSession(string session)
+        {
+            httpReq.Headers.Add("Cookie", session);
+        }
+
+        public HttpResponse request(Object obj)
+        {
+            string reqMsg = JSON.stringify(obj);
+            return request(reqMsg);
+        }
+
+        private HttpResponse request(string reqMsg)
+        {
+            Console.WriteLine("请求url：" + httpReq.RequestUri);
+            Console.WriteLine("请求内容：" + reqMsg);
+            if (method() == HttpMethod.POST)
+            {
+                Stream myRequestStream = httpReq.GetRequestStream();
+                StreamWriter myStreamWriter = new StreamWriter(myRequestStream, encoding);
+                myStreamWriter.Write(reqMsg);
+                myStreamWriter.Close();
+            }
+            response = new HttpResponse((HttpWebResponse)httpReq.GetResponse());
+            if (response.needUpdateSession(session))
+            {
+                session = response.getSession();
+            }
+            return response;
+        }
+
+        private string method()
+        {
+            return httpReq.Method;
+        }
+
+    }
+}
---
+++ PCClient/LogingWindow/ToolClass/HttpResponse.cs
@@ -0,0 +1,55 @@
+﻿using PCClintSoftware.ToolClass;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogingWindow.ToolClass
+{
+    class HttpResponse
+    {
+        private UTF8Encoding encoding = new UTF8Encoding(false);   //注明utf-8去除BOM
+        private HttpWebResponse httpResp = null;
+        private string result = null;
+
+        public HttpResponse(HttpWebResponse httpResp)
+        {
+            this.httpResp = httpResp;
+            getResultStr();
+        }
+
+        public string getSession()
+        {
+            return httpResp.Headers.Get("Set-Cookie");
+        }
+
+        public bool needUpdateSession(string oldSession)
+        {
+            return httpResp.Headers.Get("Set-Cookie") != null && !httpResp.Headers.Get("Set-Cookie").Equals(oldSession);
+        }
+
+        private void getResultStr()
+        {
+            Stream respStream = httpResp.GetResponseStream();
+            StreamReader respStreamReader = new StreamReader(respStream, encoding);
+            result = respStreamReader.ReadToEnd();
+        }
+        public string getResult()
+        {
+            return result;
+        }
+
+        public T getResultAsObj<T>()
+        {
+            return JSON.parse<T>(result); ;
+        }
+
+        public List<T> getResultAsObjList<T>()
+        {
+            return JSON.parse<List<T>>(result);;
+        }
+    }
+}
